98b8bd545abb4e3207a464e7f7d4999abbacee8e
lazarevaak (Лазарева Александра) <lazarevaak@yandex.ru>
2024-12-18 12:16:32 +0300

    Initial Commit

---
+++ aklazarevaPW5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aklazarevaPW5
+//
+//  Created by MacBoock on 18.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
